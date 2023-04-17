ef1412a8bb26e7eb358e292e6d6c5c9da977081e
loquillitas <127319512+loquillitas@users.noreply.github.com>
2023-04-17 20:29:56 -0300

    Add files via upload

---
+++ borrador/chart01.js
@@ -0,0 +1,27 @@
+d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(locale => {
+  d3.timeFormatDefaultLocale(locale)
+})
+
+  /* Agregamos al DOM la visualización chartMap */
+  d3.dsv(";",'147_vehiculos_mal_estacionados_auxiliar.csv', d3.autoType).then(data => {
+    console.log(data)
+    // Guardamos el svg generado en la variable chart
+    let chart = Plot.plot({
+      marks: [
+        Plot.line(data, 
+          Plot.groupX({ y:"count" }, { x: "hora_ingreso_aux"})
+        ),
+          
+        ],
+        x: {
+          label: null,
+          line: true,
+        },
+        y: {
+          label: null,
+          line: true,
+        },
+
+    })
+  d3.select('#chart_01').append(() => chart)
+})
